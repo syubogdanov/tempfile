118924439558026933d4c701989470a29e804df6
Sergei Bogdanov <77158975+syubogdanov@users.noreply.github.com>
2023-12-28 13:45:59 +0300

    DEV: add header guard

--- include/tempfile/tempfile.hpp
+++ include/tempfile/tempfile.hpp
@@ -1,3 +1,6 @@
+#ifndef INCLUDE_TEMPFILE_TEMPFILE_HPP_
+#define INCLUDE_TEMPFILE_TEMPFILE_HPP_
+
 #include <filesystem>
 
 namespace tempfile
@@ -8,3 +11,5 @@ std::filesystem::path mkdtemp(void);
 std::filesystem::path mkstemp(void);
 
 } // namespace tempfile
+
+#endif  // INCLUDE_TEMPFILE_TEMPFILE_HPP_
